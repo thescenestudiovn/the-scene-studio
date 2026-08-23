17725ec8d58a07b175c7a88664fd235043809711
thescenestudiovn <thescenestudiovn@gmail.com>
2026-08-23 21:41:19 +0700

    Add draggable cover editor to Gallery collection admin

--- app/admin/gallery/[id]/page.tsx
+++ app/admin/gallery/[id]/page.tsx
@@ -1,1 +1,13 @@
-export { default } from "../../collections/[id]/page";
+"use client";
+
+import { useParams } from "next/navigation";
+import AdminCollectionEditor from "../../../../admin/collections/[id]/page";
+import CoverPositionEditor from "../../../../admin/components/CoverPositionEditor";
+
+export default function AdminGalleryCollectionPage() {
+  const { id } = useParams<{ id: string }>();
+  return <>
+    <AdminCollectionEditor />
+    {id && <div className="mx-auto max-w-7xl px-6 pb-16 md:px-10"><CoverPositionEditor collectionId={id} /></div>}
+  </>;
+}
